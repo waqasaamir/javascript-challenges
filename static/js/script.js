05d66d03241502c5f1eb04b48aaf1f16ad31edb4
Waqas <waqasaamir515@gmail.com>
2022-02-16 15:24:17 +0500

    git commit

--- static/js/script.js
+++ static/js/script.js
@@ -160,12 +160,46 @@ function buttonsRandom(){
 }
 //Challenge 5
 let blackjackGame={
-    'you':{'scoreSpan':'#your-blackjack-score','div':'#your-box','score':0},
-    'dealer':{'scoreSpan':'#dealer-blackjack-score','div':'#dealer-box','score':0}
-}
+    'you':{'scoreSpan':'#your-blackjack-result','div':'#your-box','score':0},
+    'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box','score':0},
+    'cards':['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+
+};
+const YOU= blackjackGame['you'];
+const DEALER= blackjackGame['dealer'];
+
+const hitSound=new Audio('static/sounds/swish.m4a');
+
 
 document.querySelector('#blacjack-hit-button').addEventListener('click',blackjackhit);
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackDeal);
 
 function blackjackhit(){
-    alert('Daddy, Harder!');
+let card= randomCard();
+console.log(card);
+showCard(DEALER);
+ showCard(YOU);
+}
+function randomCard(){
+    let randomIndex= Math.floor(Math.random()*13);
+    return blackjackGame['cards'][randomIndex];
+}
+function showCard(activePlayer){
+    let cardImage= document.createElement('img');
+    cardImage.src='static/images/A.png';
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+function blackjackDeal(){
+    let yourImages= document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages=document.querySelector('#dealer-box').querySelectorAll('img');
+
+    for(let i=0; i < yourImages.length; i++)
+    {
+        yourImages[i].remove();
+    } 
+    for(let i=0; i < dealerImages.length; i++)
+    {
+        dealerImages[i].remove();
+    }
 }
